f9381234b097ecad970ec0503d679a60e14d47e3
Surafel Kassahun <surafelkassahun21@gmail.com>
2025-03-22 11:47:46 +0300

    update calendar

--- src/Config/CalendarManagment.ts
+++ src/Config/CalendarManagment.ts
@@ -1,22 +1,42 @@
 import { startOfDay } from 'date-fns';
 import { UserSettingModel } from "../model/userSetting";
 import { CalendarEventModel } from '../model/calendar';
+import { UserAccountModel } from '../model/userAccounts';
+
+export const checkUserAvailability = async (userId:string, startDate: Date, endDate:Date): Promise<{ isAvailable: boolean; message: string }  > => {
+    try {
+        const checkCalendar = await CheckUserCalendar(userId, startDate, endDate)
+        const checkSchedule = await checkScheduleAvailable(userId, startDate, endDate)
+
+        const userIsAvailable:boolean = checkCalendar.isAvailable && checkSchedule.isAvailable ? true : false
+
+        return { isAvailable: userIsAvailable, message : `${checkCalendar.message} and ${checkSchedule.message} ` }
+    } catch (error){
+        return {  isAvailable: false,
+            message: "An error occurred while checking availability." }
+    }
+}
 
 export const CheckUserCalendar = async (userId:string, startDate: Date, endDate:Date): Promise<{ isAvailable: boolean; message: string }  > => {
     try {
+
+        const userEmails = await UserAccountModel.find({
+            userId: userId
+        })
+        const userEmailIds = await userEmails.map(({_id}) => _id )
         const userCheckCalendar = await CalendarEventModel.find({
-            userId:userId
+            userId:{$in: userEmailIds},
+            startTime: {$gte: startDate },
+            endTime: {$lte: endDate}
         })
 
         if(!userCheckCalendar){
            return {isAvailable:true, message:`Calendar is free at ${startDate} - ${endDate}` }
         }
 
-        // const calendar = userCheckCalendar.
-
-        return {isAvailable:true, message:`Available on ${startDate} - ${endDate}` }
+        return {isAvailable:true, message:`User calendar is Available on ${startDate} - ${endDate}` }
     } catch (error) {
-        return {isAvailable:true, message:`Available on ${startDate} - ${endDate}` }
+        return {isAvailable:true, message:`Error in checking calendar ${startDate} - ${endDate}` }
     }
 }
 
@@ -39,9 +59,9 @@ export const checkScheduleAvailable = async (userId:string, startDate: Date, end
         const endDay = dayNames[endDate.getDay()];
 
         if (!userWorkingDays || !userWorkingDays.includes(startDay) || !userWorkingDays.includes(endDay) ){
-            return {isAvailable: false, message : `User is not available on ${startDate} - ${endDate} ` }
+            return {isAvailable: false, message : `User preference is not on ${startDate} - ${endDate} ` }
         }
-        return {isAvailable:true, message:`Available on ${startDate} - ${endDate}` }
+        return {isAvailable:true, message:`User preference is ${startDate} - ${endDate}` }
     } catch (error) {
         return {
             isAvailable: false,
